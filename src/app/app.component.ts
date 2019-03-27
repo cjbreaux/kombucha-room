723ec6baef1721510539b68b594aae8e8b2c0156
Chris Breaux and Nathan Nielson <nathan@>
2019-03-27 10:21:03 -0700

    display kombucha data, add method to update list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Kombucha } from './model/kombucha.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'The Booch Room!';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  kegList : Kombucha[] = [
+    new Kombucha ('Radiate', 'Soma',5.50 , 'Cherry Chai'),
+    new Kombucha ('Thrive', 'Soma',3.50 , 'Lemon Ginger'),
+
+    new Kombucha ('Uplift', 'Brew DR',6.50 , 'Peppermint Green Tea'),
+    new Kombucha ('Superberry', 'Brew DR',7.50 , 'Elderberry Hibiscus'),
+
+    new Kombucha ('Coconut lime', 'humm',5.50 , 'Coconut lime'),
+    new Kombucha ('Ginger Juniper', 'humm',6.50 , 'Ginger Juniper')
+  ];
+  addNewBooch(name: string, brand: string, price: number, flavor: string): void {
+    this.kegList.push(new Kombucha(name,brand,price,flavor));
+  }
 }
---
+++ src/app/model/kombucha.model.ts
@@ -0,0 +1,4 @@
+export class Kombucha {
+  constructor(public name : string, public brand : string, public price : number, public flavor : string)
+  {}
+}
